d0ed9cd77f48302290ddacfe1dde9f4a04001e8a
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-02 00:54:23 -0500

    added content to convert_to_months() and main

--- intro_to_functions/main_functions.py
+++ intro_to_functions/main_functions.py
@@ -1,5 +1,10 @@
-def convert_to_months(x):
-  pass
+def convert_to_months(months):
+  months_in_a_year = 12
+  years = months_in_a_year / months
+  return years
 
 if __name__ == "__main__":
-    
+    months_from_user = input("Enter age in months: ")
+    age_in_months = int(months_from_user)
+    age_in_years = convert_to_months(age_in_months)
+    print(f'{age_in_years}' + " is " + f'{age_in_months}' + " months.")
