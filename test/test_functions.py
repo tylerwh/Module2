1e71d7deb3a28653c69684ea3ace130501edf709
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-02 00:37:19 -0500

    changed test_increase() - test fails

--- test/test_functions.py
+++ test/test_functions.py
@@ -4,7 +4,7 @@ from intro_to_functions import main_functions
 
 class FunctionTestCase(unittest.TestCase):
   def test_increase(self):
-    self.assertEqual(False, True)
+    self.assertEqual(main_functions.convert_to_months(12), 1)
 
 
 if __name__ == "__main__":
