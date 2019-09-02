279855df9294bfc29a07416841ef518de6bfa650
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-02 00:26:08 -0500

    Initial commit

---
+++ intro_to_functions/main_functions.py
@@ -0,0 +1,5 @@
+def convert_to_months(x):
+  pass
+
+if __name__ == "__main__":
+    
---
+++ test/test_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from intro_to_functions import main_functions
+
+
+class FunctionTestCase(unittest.TestCase):
+  def test_increase(self):
+    self.assertEqual(False, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
